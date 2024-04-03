d2baeaf0b62ee906567f1fac513f8a68ff995bc8
lesss.prn <122449803+NikitaPetrosienko@users.noreply.github.com>
2024-04-04 01:32:04 +0300

    Hard task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,56 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyChange: UITextView!
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var outletBotton: UIButton!
-    var countTouch = 0
     
     
-    @IBAction func botton(_ sender: UIButton) {
-        
-        countTouch += 1
-        counterLabel.text = "Значение счетчика \(countTouch)"
-        
+    func appendToHistoryChange(message: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+
+        storyChange.text += "\n[\(dateString)]: \(message)"
+        let range = NSMakeRange(storyChange.text.count - 1, 1)
+        storyChange.scrollRangeToVisible(range)
     }
     
+    var countTouch = 0 {
+           didSet {
+               updateCounterLabel()
+           }
+       }
+
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           counterLabel.text = "Значение счетчика \(countTouch)"
+           counterLabel.adjustsFontSizeToFitWidth = true
+           counterLabel.minimumScaleFactor = 0.5
+           counterLabel.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+           counterLabel.textColor = UIColor.white
+           counterLabel.textAlignment = .center
+           counterLabel.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+           counterLabel.layer.cornerRadius = 10
+           counterLabel.clipsToBounds = true
+       }
+
+       @IBAction func plusCount(_ sender: UIButton) {
+           updateCountTouch(action: .increment)
+           appendToHistoryChange(message: "значение изменено на +1")
+       }
+
+       @IBAction func minusCount(_ sender: UIButton) {
+           updateCountTouch(action: .decrement)
+           
+       }
+
+       @IBAction func breakCounter(_ sender: UIButton) {
+           updateCountTouch(action: .reset)
+           appendToHistoryChange(message: "значение сброшено")
+       }
+       
+       func updateCounterLabel() {
+           counterLabel.text = "Значение счетчика \(countTouch)"
+       }
+
+       func updateCountTouch(action: CounterAction) {
+           switch action {
+           case .increment:
+               countTouch += 1
+           case .decrement:
+               if countTouch > 0 {
+                   countTouch -= 1
+                   appendToHistoryChange(message: "значение изменено на -1")
+               } else {
+                   appendToHistoryChange(message: "попытка уменьшить значение счётчика ниже 0")
+               }
+           case .reset:
+               countTouch = 0
+           }
+       }
+       
+       enum CounterAction {
+           case increment
+           case decrement
+           case reset
+       }
+       
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        counterLabel.adjustsFontSizeToFitWidth = true
-        counterLabel.minimumScaleFactor = 0.5
-        counterLabel.text = "Значение счетчика \(countTouch)"
-        counterLabel.font = UIFont.systemFont(ofSize: 24, weight: .medium)
-        counterLabel.textColor = UIColor.white
-        counterLabel.textAlignment = .center
-        counterLabel.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        counterLabel.layer.cornerRadius = 10
-        counterLabel.clipsToBounds = true
-        
-        
-        outletBotton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            
-            outletBotton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            outletBotton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            outletBotton.widthAnchor.constraint(equalToConstant: 200),
-            outletBotton.heightAnchor.constraint(equalToConstant: 50)
-        ])
-
-            outletBotton.setTitle("Тапни меня", for: .normal)
-            outletBotton.titleLabel?.font = UIFont.systemFont(ofSize: 26, weight: .semibold)
-            outletBotton.setTitleColor(UIColor.white, for: .normal)
-            outletBotton.backgroundColor = UIColor.systemBrown
-            outletBotton.layer.cornerRadius = 20
-            outletBotton.layer.shadowColor = UIColor.darkGray.cgColor
-            outletBotton.layer.shadowOffset = CGSize(width: 0, height: 2)
-            outletBotton.layer.shadowOpacity = 0.5
-            outletBotton.layer.shadowRadius = 2
-        
-        
-        
-    }
-
 
 }
 
