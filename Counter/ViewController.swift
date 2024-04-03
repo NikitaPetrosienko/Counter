b298d45b051f2d3e5ddea1c7da1a1895b3027b8a
lesss.prn <122449803+NikitaPetrosienko@users.noreply.github.com>
2024-04-04 00:18:14 +0300

    Base task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var outletBotton: UIButton!
+    var countTouch = 0
+    
+    
+    @IBAction func botton(_ sender: UIButton) {
+        
+        countTouch += 1
+        counterLabel.text = "Значение счетчика \(countTouch)"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.adjustsFontSizeToFitWidth = true
+        counterLabel.minimumScaleFactor = 0.5
+        counterLabel.text = "Значение счетчика \(countTouch)"
+        counterLabel.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+        counterLabel.textColor = UIColor.white
+        counterLabel.textAlignment = .center
+        counterLabel.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        counterLabel.layer.cornerRadius = 10
+        counterLabel.clipsToBounds = true
+        
+        
+        outletBotton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            
+            outletBotton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            outletBotton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            outletBotton.widthAnchor.constraint(equalToConstant: 200),
+            outletBotton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+            outletBotton.setTitle("Тапни меня", for: .normal)
+            outletBotton.titleLabel?.font = UIFont.systemFont(ofSize: 26, weight: .semibold)
+            outletBotton.setTitleColor(UIColor.white, for: .normal)
+            outletBotton.backgroundColor = UIColor.systemBrown
+            outletBotton.layer.cornerRadius = 20
+            outletBotton.layer.shadowColor = UIColor.darkGray.cgColor
+            outletBotton.layer.shadowOffset = CGSize(width: 0, height: 2)
+            outletBotton.layer.shadowOpacity = 0.5
+            outletBotton.layer.shadowRadius = 2
+        
+        
+        
     }
 
 
